7f541111f1293bef68d448bd66834fc2400b6e4c
NitzanBen24 <nitzanben24@gmail.com>
2024-11-06 23:20:52 +0200

    supabase env varibales remove use

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL!;
-const supabaseAnonKey = process.env.SUPABASE_API_KEY!;
+
+const supabaseUrl = 'https://khcvcqvpsrlxneziwmsx.supabase.co';//process.env.SUPABASE_PROJECT_URL!;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY3ZjcXZwc3JseG5leml3bXN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNzIxOTQsImV4cCI6MjA0Mzk0ODE5NH0.KP623OklxMERe9dp3Udp0yYJTD7ZEBUqLSq3geG_dOM';//process.env.SUPABASE_API_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
