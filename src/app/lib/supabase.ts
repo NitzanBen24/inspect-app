031c9f9043fe29c8af1b8c08465e3353f7c8438d
NitzanBen24 <nitzanben24@gmail.com>
2025-02-23 19:05:52 +0200

    V7: undo supabase changes

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseKey = process.env.SUPABASE_API_KEY || '';
+const supabaseUrl = 'https://khcvcqvpsrlxneziwmsx.supabase.co';//process.env.SUPABASE_URL || '';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY3ZjcXZwc3JseG5leml3bXN4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODM3MjE5NCwiZXhwIjoyMDQzOTQ4MTk0fQ.NJ3zNGsCO2AkwvgSQ-XTCzysXHrMgZGqUK87bHM3d3U';//process.env.SUPABASE_API_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
