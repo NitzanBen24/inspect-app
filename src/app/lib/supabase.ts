8459708dd501073a00c370609f69ce50a8b1beef
NitzanBen24 <nitzanben24@gmail.com>
2025-02-23 18:54:38 +0200

    V7: supabase update

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = 'https://khcvcqvpsrlxneziwmsx.supabase.co';//process.env.SUPABASE_PROJECT_URL!;
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY3ZjcXZwc3JseG5leml3bXN4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODM3MjE5NCwiZXhwIjoyMDQzOTQ4MTk0fQ.NJ3zNGsCO2AkwvgSQ-XTCzysXHrMgZGqUK87bHM3d3U';
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_API_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
